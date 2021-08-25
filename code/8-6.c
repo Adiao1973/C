63e6a1a93407aac030094dde366d6f9a439a7bd6
Adiao1973 <shangyuanjiezm@outlook.com>
2021-08-25 21:49:38 +0800

    do...while 循环

---
+++ code/8-6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    /* 局部变量定义 */
+    int a = 100;
+    int b = 200;
+
+    switch (a)
+    {
+        case 100:
+            printf("这是外部 switch 的一部分\n");
+            switch(b) {
+                case 200:
+                    printf("这个内部 switch 的一部分\n");
+            }
+            break;
+        
+        default:
+            break;
+    }
+    printf("a 的准确值是 %d\n", a);
+    printf("b 的准确值是 %d\n", b);
+
+    return 0;
+}
